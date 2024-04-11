cd54adf9eeae3d201284f43280d125d14a278b18
Luis Enrique Leon Hernandez <>
2024-04-11 11:28:17 -0600

    add migraciones tabla observaciones, estatus y seeder observaciones

--- api-pueblos-magicos/database/seeders/DatabaseSeeder.php
+++ api-pueblos-magicos/database/seeders/DatabaseSeeder.php
@@ -26,5 +26,6 @@ class DatabaseSeeder extends Seeder
         $this->call(PueblosMagicosSeeder::class);
         $this->call(TiposImagenesSeeder::class);
         $this->call(UsuarioAdminSeeder::class);
+        $this->call(Status::class);
     }
 }
