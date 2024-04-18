232b0d59ec9fc409bebffe5d75761fac67d77280
Luis Enrique Leon Hernandez <>
2024-04-18 13:01:00 -0600

    env example

--- api-pueblos-magicos/routes/api.php
+++ api-pueblos-magicos/routes/api.php
@@ -16,24 +16,24 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    Route::get('servicios',[ServiciosController::class,'getAllPreview']);
-    Route::get('/servicios/{id}',[ServiciosController::class,'show']);
-    Route ::get('servicios/filtrar/estatus/{id_estatus}',[ServiciosController::class,'getServiciosByEstatus']);
-    Route ::get('servicios/filtrar/pueblo/magico/{id_pueblo}',[ServiciosController::class,'getServiciosByPueblo']);
-    Route ::get('servicios/filtrar/categoria/{id_categoria}',[ServiciosController::class,'getServiciosByCategoria']);
-    Route ::get('servicios/filtrar/{id_estatus}/{id_pueblo}/{id_categoria}',[ServiciosController::class,'getServiciosFiltradoEspecifico']);
-    Route::post('users/logout',[UserController::class,'logout']);
-    Route::post('servicios/registrar',[ServiciosController::class,'store']);
-    Route::post('observaciones',[ObservacionesController::class,'store']);
-    Route::delete('/servicios/{servicio}',[ServiciosController::class,'destroy']);
-    Route::put('servicios/{servicio}',[ServiciosController::class,'updateServicio']);
+    Route::get('servicios', [ServiciosController::class, 'getAllPreview']);
+    Route::get('/servicios/{id}', [ServiciosController::class, 'show']);
+    Route::get('servicios/filtrar/estatus/{id_estatus}', [ServiciosController::class, 'getServiciosByEstatus']);
+    Route::get('servicios/filtrar/pueblo/magico/{id_pueblo}', [ServiciosController::class, 'getServiciosByPueblo']);
+    Route::get('servicios/filtrar/categoria/{id_categoria}', [ServiciosController::class, 'getServiciosByCategoria']);
+    Route::get('servicios/filtrar/{id_estatus}/{id_pueblo}/{id_categoria}', [ServiciosController::class, 'getServiciosFiltradoEspecifico']);
+    Route::post('users/logout', [UserController::class, 'logout']);
+    Route::post('servicios/registrar', [ServiciosController::class, 'store']);
+    Route::post('observaciones', [ObservacionesController::class, 'store']);
+    Route::delete('/servicios/{servicio}', [ServiciosController::class, 'destroy']);
+    Route::put('servicios/{servicio}', [ServiciosController::class, 'updateServicio']);
 });
 
-Route::get('catestados',[StateController::class,'index']);
-Route::get('cattiposUsers',[TipoUserController::class,'index']);
-Route::get('catservicios',[CatServiciosController::class,'index']);
-Route::get('tiposervicios',[TiposServiciosController::class,'index']);
-Route::get('pueblosmagicos',[PueblosMagicosController::class,'index']);
+Route::get('catestados', [StateController::class, 'index']);
+Route::get('cattiposUsers', [TipoUserController::class, 'index']);
+Route::get('catservicios', [CatServiciosController::class, 'index']);
+Route::get('tiposervicios', [TiposServiciosController::class, 'index']);
+Route::get('pueblosmagicos', [PueblosMagicosController::class, 'index']);
 
-Route::post('users/registrar',[UserController::class,'create']);
-Route::post('users/login',[UserController::class,'login']);
+Route::post('users/registrar', [UserController::class, 'create']);
+Route::post('users/login', [UserController::class, 'login']);
