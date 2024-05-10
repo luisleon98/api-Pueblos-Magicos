d64c33dec9fc1add93979992505fa842d50d4de5
Luis Enrique Leon Hernandez <>
2024-05-10 11:20:20 -0600

    add modelo Festividades

--- api-v10/app/Models/FestividadesImagenes.php
+++ api-v10/app/Models/FestividadesImagenes.php
@@ -2,10 +2,32 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+/**
+ * @OA\Schema(
+ *     schema="FestividadesImagenes",
+ *     title="FestividadesImagenes",
+ *     description="Modelo para representar las Imágenes de las Festividades",
+ *     @OA\Property(
+ *         property="id_servicio",
+ *         type="integer",
+ *         example=1
+ *     ),
+ *     @OA\Property(
+ *         property="id_imagen",
+ *         type="integer",
+ *         example=1
+ *     ),
+ * )
+ */
 class FestividadesImagenes extends Model
 {
-    use HasFactory;
+    use HasFactory,SoftDeletes;
+    protected $table = 'festividades_imagenes';
+    protected $fillable = [
+        'id_servicio',
+        'id_imagen'
+    ];
 }
