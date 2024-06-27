251b552e54a993be97d1f9d49f4e6d1f32a96c75
Luis Enrique Leon Hernandez <>
2024-06-27 14:37:57 -0600

    add validar password en registro de usuarios

--- api-v10/app/Http/Requests/RegistroRequest.php
+++ api-v10/app/Http/Requests/RegistroRequest.php
@@ -28,26 +28,28 @@ class RegistroRequest extends FormRequest
             'data.nombre'=>['required','string'],
             'data.apellido_pat'=>['required','string'],
             'data.apellido_mat'=>['required','string'],
-            'data.password'=>['required','string','confirmed'],
+            'data.password' => ['required', 'string', 'confirmed', 'min:8', 'regex:/^(?=.*[A-Z])(?=.*\d).+$/'],
             
         ];
     }
     public function messages()
-    {
-        return[
-            'data.user_name.required' => 'El nombre de usuario es obligatorio',
-            'data.user_name.email' => 'El nombre de usuario debe ser un correo electrónico válido',
-            'data.nombre.required' => 'El nombre es obligatorio',
-            'data.nombre.string' => 'El nombre debe ser una cadena de texto',
-            'data.apellido_pat.required' => 'El apellido paterno es obligatorio',
-            'data.apellido_pat.string' => 'El apellido paterno debe ser una cadena de texto',
-            'data.apellido_mat.required' => 'El apellido materno es obligatorio',
-            'data.apellido_mat.string' => 'El apellido materno debe ser una cadena de texto',
-            'data.password.required' => 'La contraseña es obligatoria',
-            'data.password.string' => 'La contraseña debe ser una cadena de texto',
-            'data.password.confirmed' => 'Debes confirmar la contraseña',
-        ];
-    }
+{
+    return [
+        'data.user_name.required' => 'El nombre de usuario es obligatorio',
+        'data.user_name.email' => 'El nombre de usuario debe ser un correo electrónico válido',
+        'data.nombre.required' => 'El nombre es obligatorio',
+        'data.nombre.string' => 'El nombre debe ser una cadena de texto',
+        'data.apellido_pat.required' => 'El apellido paterno es obligatorio',
+        'data.apellido_pat.string' => 'El apellido paterno debe ser una cadena de texto',
+        'data.apellido_mat.required' => 'El apellido materno es obligatorio',
+        'data.apellido_mat.string' => 'El apellido materno debe ser una cadena de texto',
+        'data.password.required' => 'La contraseña es obligatoria',
+        'data.password.string' => 'La contraseña debe ser una cadena de texto',
+        'data.password.confirmed' => 'Debes confirmar la contraseña',
+        'data.password.min' => 'La contraseña debe tener al menos 8 caracteres',
+        'data.password.regex' => 'La contraseña debe contener al menos una mayúscula y un número',
+    ];
+}
     protected function failedValidation(Validator $validator) {
         throw new HttpResponseException(response()->json([
             'data' => $validator->errors(),
