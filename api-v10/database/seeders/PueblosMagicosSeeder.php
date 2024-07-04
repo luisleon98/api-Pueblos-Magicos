6700cb13f1c6be218038d812f12d9f9198b8fc54
Luis Enrique Leon Hernandez <>
2024-07-04 12:12:01 -0600

    se agregan 2 pueblos magicos y los años de los pueblos magicos

--- api-v10/database/seeders/PueblosMagicosSeeder.php
+++ api-v10/database/seeders/PueblosMagicosSeeder.php
@@ -12,17 +12,31 @@ class PueblosMagicosSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-    {
-        $pueblos = ['Cholula','Atlixco','Tetela de Ocampo','Zacatlán','Xicotepec','Tlatlauquitepec','Pahuatlán','Huachinango','Chignahuapan','Cuetzalan'];
-        foreach ($pueblos as $index => $pueblo) {
-            DB::table('pueblos_magicos')->insert([
-                'nombre' => $pueblo,
-                'descripcion' => '',
-                'id_direccion' => $index + 1,
-                'year'=> 2017,
-                'created_at' => now(),
-                'updated_at' => now(),
-            ]);
-        }
+{
+    $pueblos = [
+        ['nombre' => 'Cuetzalan', 'year' => 2002],
+        ['nombre' => 'Zacatlán', 'year' => 2011],
+        ['nombre' => 'Pahuatlán', 'year' => 2012],
+        ['nombre' => 'Cholula', 'year' => 2012],
+        ['nombre' => 'Chignahuapan', 'year' => 2012],
+        ['nombre' => 'Tlatlauquitepec', 'year' => 2012],
+        ['nombre' => 'Xicotepec', 'year' => 2012],
+        ['nombre' => 'Atlixco', 'year' => 2015],
+        ['nombre' => 'Huauchinango', 'year' => 2015],
+        ['nombre' => 'Tetela de Ocampo', 'year' => 2020],
+        ['nombre' => 'Huejotzingo', 'year' => 2023],
+        ['nombre' => 'Teziutlán', 'year' => 2023],
+    ];
+
+    foreach ($pueblos as $index => $pueblo) {
+        DB::table('pueblos_magicos')->insert([
+            'nombre' => $pueblo['nombre'],
+            'descripcion' => '',
+            'id_direccion' => $index + 1,
+            'year' => $pueblo['year'],
+            'created_at' => now(),
+            'updated_at' => now(),
+        ]);
     }
+}
 }
