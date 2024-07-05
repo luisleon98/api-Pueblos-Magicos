c6e4d4f8103bf6e1dae9942f7c2c2ee43fbe571f
Luis Enrique Leon Hernandez <>
2024-07-05 12:24:43 -0600

    image service fixed con storage disk

--- api-v10/app/Http/Controllers/ImagenController.php
+++ api-v10/app/Http/Controllers/ImagenController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Imagen;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\File;
+use Illuminate\Support\Facades\Storage;
 
 class ImagenController extends Controller
 {
@@ -36,25 +37,29 @@ class ImagenController extends Controller
      * Display the specified resource.
      */
     public function show($id)
-    {
-        $imagen = Imagen::find($id);
+{
+    $imagen = Imagen::find($id);
 
-        if (!$imagen) {
-            return response()->json([
-                'data'=>["error" => "Imagen no encontrada"]
-            ], 404);
-        }
-        $path = storage_path(env('STORAGE_PATH', '../public/uploads/') . $imagen->nombre);
-        if (File::exists($path)) {
-            $contenido = file_get_contents($path);
-            $imagenb64 = base64_encode($contenido);
-        } else {
-            $imagenb64 = null;
-        }
+    if (!$imagen) {
         return response()->json([
-            "data" => ["imagen" => $imagenb64]
-        ]);
+            'data' => ["error" => "Imagen no encontrada"]
+        ], 404);
     }
+
+    $disk = Storage::disk('custom_uploads');
+
+    if ($disk->exists($imagen->nombre)) {
+        $contenido = $disk->get($imagen->nombre);
+        $imagenb64 = base64_encode($contenido);
+    } else {
+        $imagenb64 = null;
+        
+    }
+
+    return response()->json([
+        "data" => ["imagen" => $imagenb64]
+    ]);
+}
 
     /**
      * Show the form for editing the specified resource.
