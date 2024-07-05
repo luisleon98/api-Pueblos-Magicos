b6e4840fa3c05af8f91f5efc26e4bf59d7ca3604
Luis Enrique Leon Hernandez <>
2024-07-05 15:25:59 -0600

    se agrego el nombre clima para consumir el api de clima con el nombre completo

--- api-v10/database/migrations/2024_03_19_223217_create_pueblos_magicos_table.php
+++ api-v10/database/migrations/2024_03_19_223217_create_pueblos_magicos_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('nombre');
             $table->string('descripcion');
+            $table->string('nombre_clima');
             $table->integer('year');
             $table->foreignId('id_direccion')->constrained('direcciones','id');
             $table->timestamps();
--- api-v10/database/seeders/PueblosMagicosSeeder.php
+++ api-v10/database/seeders/PueblosMagicosSeeder.php
@@ -12,31 +12,32 @@ class PueblosMagicosSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void
-{
-    $pueblos = [
-        ['nombre' => 'Cuetzalan', 'year' => 2002],
-        ['nombre' => 'Zacatlán', 'year' => 2011],
-        ['nombre' => 'Pahuatlán', 'year' => 2012],
-        ['nombre' => 'Cholula', 'year' => 2012],
-        ['nombre' => 'Chignahuapan', 'year' => 2012],
-        ['nombre' => 'Tlatlauquitepec', 'year' => 2012],
-        ['nombre' => 'Xicotepec', 'year' => 2012],
-        ['nombre' => 'Atlixco', 'year' => 2015],
-        ['nombre' => 'Huauchinango', 'year' => 2015],
-        ['nombre' => 'Tetela de Ocampo', 'year' => 2020],
-        ['nombre' => 'Huejotzingo', 'year' => 2023],
-        ['nombre' => 'Teziutlán', 'year' => 2023],
-    ];
+    {
+        $pueblos = [
+            ['nombre' => 'Cuetzalan', 'year' => 2002, 'nombre_clima' => 'Cuetzalan'],
+            ['nombre' => 'Zacatlán', 'year' => 2011, 'nombre_clima' => 'Zacatlan'],
+            ['nombre' => 'Pahuatlán', 'year' => 2012, 'nombre_clima' => 'Pahuatlan de Valle'],
+            ['nombre' => 'Cholula', 'year' => 2012, 'nombre_clima' => 'Cholula'],
+            ['nombre' => 'Chignahuapan', 'year' => 2012, 'nombre_clima' => 'Chignahuapan'],
+            ['nombre' => 'Tlatlauquitepec', 'year' => 2012, 'nombre_clima' => 'Tlatlauquitepec'],
+            ['nombre' => 'Xicotepec', 'year' => 2012, 'nombre_clima' => 'Xicotepec de juarez'],
+            ['nombre' => 'Atlixco', 'year' => 2015, 'nombre_clima' => 'Atlixco'],
+            ['nombre' => 'Huauchinango', 'year' => 2015, 'nombre_clima' => 'Huauchinango'],
+            ['nombre' => 'Tetela de Ocampo', 'year' => 2020, 'nombre_clima' => 'Tetela de Ocampo'],
+            ['nombre' => 'Huejotzingo', 'year' => 2023, 'nombre_clima' => 'Huejotzingo'],
+            ['nombre' => 'Teziutlán', 'year' => 2023, 'nombre_clima' => 'Teziutlan'],
+        ];
 
-    foreach ($pueblos as $index => $pueblo) {
-        DB::table('pueblos_magicos')->insert([
-            'nombre' => $pueblo['nombre'],
-            'descripcion' => '',
-            'id_direccion' => $index + 1,
-            'year' => $pueblo['year'],
-            'created_at' => now(),
-            'updated_at' => now(),
-        ]);
+        foreach ($pueblos as $index => $pueblo) {
+            DB::table('pueblos_magicos')->insert([
+                'nombre' => $pueblo['nombre'],
+                'descripcion' => '',
+                'nombre_clima' => $pueblo['nombre_clima'],
+                'id_direccion' => $index + 1,
+                'year' => $pueblo['year'],
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]);
+        }
     }
-}
 }
