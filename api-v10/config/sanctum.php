2287d045ca93890faa197b4ceea26672a090b0b8
Luis Enrique Leon Hernandez <>
2024-07-10 11:27:16 -0600

    cors modificaciones local

--- api-v10/config/sanctum.php
+++ api-v10/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env('SANCTUM_STATEFUL_DOMAINS', sprintf(
         '%s%s',
-        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,gestor.dsi.ipn.mx',
+        'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,gestor.dsi.ipn.mx,localhost:5174',
         Sanctum::currentApplicationUrlWithPort()
     ))),
 
