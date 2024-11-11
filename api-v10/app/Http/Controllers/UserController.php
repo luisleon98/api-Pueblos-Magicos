03f1119310b7a5c01ab20c4c638cd4e88562ec67
Luis Enrique Leon Hernandez <>
2024-11-11 14:43:15 -0600

    se integra el id pueblo en la creacion de usuarios en el panel de admin

--- api-v10/app/Http/Controllers/UserController.php
+++ api-v10/app/Http/Controllers/UserController.php
@@ -337,7 +337,8 @@ class UserController extends Controller
             'user_name' => $data['user_name'],
             'password' => $data['password'],
             'id_tipo_usuario' => $data['id_tipo_usuario'] ?? 3,
-            'id_estatus' => $data['id_estatus'] ?? 7
+            'id_estatus' => $data['id_estatus'] ?? 7,
+            'id_pueblo'=>$data['id_pueblo']
         ]);
 
         $persona = Personas::create([
